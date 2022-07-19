e7b708116b9762420dcd7b3cebd47e9692a9d1c9
Elisa Renault <elisa.renault@protonmail.com>
2022-07-19 09:20:59 +0200

    Code organisation

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ require("dotenv").config();
 const app = express();
 
 const indexRouter = require('./routes/index');
+const contactRouter = require('./routes/contact');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,6 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/contact', contactRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -5,107 +5,10 @@
 const express = require('express');
 const router = express.Router();
 require('dotenv').config();
-const nodemailer = require('nodemailer');
-const multiparty = require('multiparty');
-const hcaptcha = require('hcaptcha');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {title: 'Aliant'});
-});
-
-/* GET home page. */
-router.get('/contact', function(req, res, next) {
-  res.render('contact', {title: 'Aliant'});
-});
-
-const middleware_form = (req, res, next) => {
-  let form = new multiparty.Form();
-  let data = {};
-
-  form.parse(req, function (err, fields) {
-    if (err !== null)
-      return next(err);
-
-    Object.keys(fields).forEach(function (property) {
-      data[property] = fields[property].toString();
-    });
-
-    req.formData = data;
-    next();
-  });
-};
-
-const hcaptcha_middleware_validate = (secret) => (req, res, next) => {
-  const token = req.formData && req.formData['h-captcha-response'];
-
-  // call next with an error if no token present
-  if (!token) {
-    const err = new Error('bad request - no token provided');
-    err.status = 400;
-    return next(err);
-  }
-
-  // verify the hcaptcha and continue on success
-  // call next with an error if verification errors or fails
-  return hcaptcha.verify(secret, token)
-    .then((data) => {
-      req.hcaptcha = data;
-      if (data.success) {
-        return next();
-      }
-      const err = new Error(`bad request - ${data['error-codes']}`);
-      err.status = 400;
-      return next(err);
-    })
-    .catch(next);
-};
-
-//nodemailer
-const transporter = nodemailer.createTransport({
-  host: 'auth.smtp.1and1.fr',
-  port: 587,
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASS,
-  },
-});
-
-// verify connection configuration
-transporter.verify(function (error, success) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Server is ready to take our messages');
-  }
-});
-
-//hcaptcha_middleware_validate(process.env.HCAPTCHA_SECRET_KEY)
-
-router.post('/send', middleware_form, hcaptcha_middleware_validate(process.env.HCAPTCHA_SECRET_KEY), (req, res) => {
-
-  data = req.formData;
-
-  if (data.miel !== '') {
-    data.subject = `[BOT] ${data.subject}`;
-  }
-
-  const mail = {
-    from: process.env.EMAIL,
-    to: process.env.EMAIL,
-    subject: data.subject,
-    text: `${data.firstname} ${data.lastname} <${data.email}> \n${data.message}`,
-  };
-
-  transporter.sendMail(mail, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.status(500).render('error');
-    } else {
-      res.status(200).render('sent');
-    }
-  });
-
 });
 
 module.exports = router;
